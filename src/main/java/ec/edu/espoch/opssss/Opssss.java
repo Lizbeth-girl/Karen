2e38e841afe05bb59ad66da74f48a77ab74a3b96
Lizbeth-girl <saezlizbethe6@gamail.com>
2024-11-05 10:16:10 -0500

    actualizacion

--- src/main/java/ec/edu/espoch/opssss/Opssss.java
+++ src/main/java/ec/edu/espoch/opssss/Opssss.java
@@ -12,6 +12,6 @@ public class Opssss {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Karen");
+        System.out.println("Karen tiene miedo");
     }
 }
